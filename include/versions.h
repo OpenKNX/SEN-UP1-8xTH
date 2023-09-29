f0748c3303488b22c6f082d3c8d909ac77129f59
Ing-Dom <dom@ing-dom.de>
2023-09-29 16:05:59 +0200

    builds

---
+++ include/versions.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define MAIN_Version "117fb5e"
+#define KNX_Version "f8f34c5"
+#define MODULE_Common_Version "1.0.0+8619f7b"
+#define MODULE_Common_ETS 16
+#define MODULE_LogicModule_Version "4.0.0+0931d4c"
+#define MODULE_LogicModule_ETS 64
+#define MODULE_VirtualButton_Version "4f39838"
+#define MODULE_Updater_Version "0.0.0+0fa5d39"
+#define MODULE_Updater_ETS 0
